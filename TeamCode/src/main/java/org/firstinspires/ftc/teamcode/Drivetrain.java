6b6727dc0a7e06ec2c64e4961dc8e0ecca469e1e
WyvernRobotics12889 <12889wyverns@gmail.com>
2021-11-03 17:40:44 -0700

    Added driveDistance() in Drivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 public abstract class Drivetrain extends LinearOpMode {
     Azure azure;
@@ -18,6 +19,35 @@ public abstract class Drivetrain extends LinearOpMode {
 
     public void driveDistance(double distance) {
         // 1.48 motor rotations per wheel rotation
+        int ticks = (int)(distance*1120*1.48*(4+(2/7)*Math.PI));
+
+        int leftAStart = azure.leftA.getCurrentPosition();
+        int leftBStart = azure.leftB.getCurrentPosition();
+        int rightAStart = azure.rightA.getCurrentPosition();
+        int rightBStart = azure.rightB.getCurrentPosition();
+
+        azure.leftA.setTargetPosition(leftAStart+ticks);
+        azure.leftB.setTargetPosition(leftBStart+ticks);
+        azure.rightA.setTargetPosition(rightAStart+ticks);
+        azure.rightB.setTargetPosition(rightBStart+ticks);
+
+        azure.leftA.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        azure.leftB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        azure.rightA.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        azure.rightB.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        setDrivePower(.5, .5);
+
+        while(azure.leftA.isBusy() || azure.leftB.isBusy() || azure.rightA.isBusy() || azure.rightB.isBusy()) {
+            sleep(10);
+        }
+
+        setDrivePower(0, 0);
+
+        azure.leftA.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        azure.leftB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        azure.rightA.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        azure.rightB.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
     public void setLift(boolean liftyBoi){
