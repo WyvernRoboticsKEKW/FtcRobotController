bd4fe03cbd4fc324f3e6ee79e9c664fbb4783fd7
WyvernRobotics12889 <12889wyverns@gmail.com>
2022-09-12 16:34:32 -0700

    tournament auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedDuck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedDuck.java
@@ -12,9 +12,9 @@ public class RedDuck extends Drivetrain {
         autonomousCamera();
         waitForStart();
 
-        while(!pipeline.isReady()) sleep(100);
+        while (!pipeline.isReady()) sleep(100);
 
-        double currentPoint = pipeline.getRectMidpointY()/240;
+        double currentPoint = pipeline.getRectMidpointY() / 240;
 
         if (currentPoint < .33) {
             intakeArm(1);
@@ -24,6 +24,61 @@ public class RedDuck extends Drivetrain {
             intakeArm(3);
         }
         azure.leftB.setPower(.5);
+        azure.leftA.setPower(.5);
+        azure.rightB.setPower(.5);
+        azure.rightA.setPower(.5);
+        sleep(870);
+        azure.leftB.setPower(0);
+        azure.leftA.setPower(0);
+        azure.rightB.setPower(0);
+        azure.rightA.setPower(0);
+        rotateNow(-40);
+        sleep(60);
+        azure.leftB.setPower(-.5);
+        azure.leftA.setPower(-.5);
+        azure.rightB.setPower(-.5);
+        azure.rightA.setPower(-.5);
+        sleep(1010);
+        setCarousel(-1);
+        sleep(3300);
+        setCarousel(0);
+        sleep(100);
+        azure.leftB.setPower(.5);
+        azure.leftA.setPower(.5);
+        azure.rightB.setPower(.5);
+        azure.rightA.setPower(.5);
+        sleep(1710);
+        azure.leftB.setPower(0);
+        azure.leftA.setPower(0);
+        azure.rightB.setPower(0);
+        azure.rightA.setPower(0);
+        sleep(100);
+        setIntake(-.8);
+        sleep(600);
+        azure.leftB.setPower(-.6);
+        azure.leftA.setPower(-.6);
+        azure.rightB.setPower(-.6);
+        azure.rightA.setPower(-.6);
+        sleep(650);
+        azure.leftB.setPower(0);
+        azure.leftA.setPower(0);
+        azure.rightB.setPower(0);
+        azure.rightA.setPower(0);
+        sleep(100);
+        rotateNow(35);
+        sleep(400);
+        azure.leftB.setPower(.6);
+        azure.leftA.setPower(.6);
+        azure.rightB.setPower(.6);
+        azure.rightA.setPower(.6);
+        sleep(900);
+        azure.leftB.setPower(0);
+        azure.leftA.setPower(0);
+        azure.rightB.setPower(0);
+        azure.rightA.setPower(0);
+
+        stopRecording(); // turn off the robots camera*/
+        /*azure.leftB.setPower(.5);
         azure.leftA.setPower(.5);
         azure.rightB.setPower(.5);
         azure.rightA.setPower(.5);
@@ -48,5 +103,7 @@ public class RedDuck extends Drivetrain {
         setIntake(0);
 
         stopRecording(); // turn off the robots camera
+
+         */
     }
 }
